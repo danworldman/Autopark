92116fcd00ee3476a63d5e6644ac7f42a474be16
Daniil Pachko <danila.pachko@gmail.com>
2022-09-13 09:35:00 +0300

    made separate init and print methods

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,52 @@
 public class Main {
     public static void main(String[] args) {
-        VehicleType[] types = new VehicleType[4];
-        types[0] = (new VehicleType("Bus", 1.2));
-        types[1] = (new VehicleType("Car", 1.0));
-        types[2] = (new VehicleType("Rink", 1.5));
-        types[3] = (new VehicleType("Tractor", 1.2));
+        VehicleType types[] = initialization();
 
         types[3].setCoeffForTax(1.3);
 
+        double maxCoeffForTax = findMax(types);
+        printMaxCoeffForTax(maxCoeffForTax);
+
+        double AverageCoeffForTax = findAverage(types);
+        printAverageCoeffForTax(AverageCoeffForTax);
+    }
+
+    public static VehicleType[] initialization() {
+        VehicleType[] arrayOfTypes = new VehicleType[4];
+        arrayOfTypes[0] = (new VehicleType("Bus", 1.2));
+        arrayOfTypes[1] = (new VehicleType("Car", 1.0));
+        arrayOfTypes[2] = (new VehicleType("Rink", 1.5));
+        arrayOfTypes[3] = (new VehicleType("Tractor", 1.2));
+
+        return arrayOfTypes;
+    }
+
+    public static double findMax(VehicleType types[]) {
         double max = types[0].coeffForTax;
-        double sum = 0.0;
         for (VehicleType v : types) {
             v.display();
             max = v.coeffForTax > max ? v.coeffForTax : max;
+        }
+
+        return max;
+    }
+
+    public static void printMaxCoeffForTax(double max) {
+        System.out.println(max);
+    }
+
+    public static double findAverage(VehicleType types[]) {
+        double sum = 0.0;
+        for (VehicleType v : types) {
+            v.display();
             sum += v.coeffForTax;
         }
-        System.out.println(max);
         double average = sum / types.length;
+
+        return average;
+    }
+
+    public static void printAverageCoeffForTax(double average) {
         System.out.println(average);
     }
 }
